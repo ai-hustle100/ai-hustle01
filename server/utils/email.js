c2c5a9d405c6e3f5fe162280d374c133edb1a80e
Gaurav Dotiyal <gauravdotiyal33@gmail.com>
2026-04-12 16:55:30 +0530

    Change to normal

--- server/utils/email.js
+++ server/utils/email.js
@@ -16,7 +16,9 @@ const sendOTPEmail = async (email, otp, name) => {
 
   try {
     const transporter = nodemailer.createTransport({
-      service: 'gmail',
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      secure: false,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
